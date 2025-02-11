0427946d04d12777671be47c64e45c54ab99d2df
Joaquin Rodriguez <jrhuerta@alayacare.com>
2025-02-11 12:15:22 -0500

    cmake from command line.

---
+++ build/CMakeFiles/indi_rpi_pb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/indi_rpi_pb.dir/rpi_powerbox.cpp.o"
+  "CMakeFiles/indi_rpi_pb.dir/rpi_powerbox.cpp.o.d"
+  "indi_rpi_pb"
+  "indi_rpi_pb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/indi_rpi_pb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
